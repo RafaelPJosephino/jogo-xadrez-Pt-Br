a6f30cf540493dbeb45c590227859c9a88b4aab0
Rafael P Jpsephino <Rafael.puckoski.josephino@gmail.com>
2021-08-20 20:37:26 -0300

    implementacao classe posicao

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,10 @@
+package aplicacao;
+
+import jogoTabuleiro.Posicao;
+
+public class Programa {
+    public static void main(String[] args) {
+        Posicao posicao = new Posicao(8,2);
+        System.out.println(posicao);
+    }
+}
