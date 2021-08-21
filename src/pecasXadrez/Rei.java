b25dfbaf1b345be400f5baa02afd09b96e4aec34
Rafael P Jpsephino <Rafael.puckoski.josephino@gmail.com>
2021-08-21 17:45:52 -0300

    Implementacao da jogada Rock Grande e Pequeno

--- src/pecasXadrez/Rei.java
+++ src/pecasXadrez/Rei.java
@@ -3,12 +3,17 @@ package pecasXadrez;
 import jogoTabuleiro.Posicao;
 import jogoTabuleiro.Tabuleiro;
 import xadrez.Cor;
+import xadrez.PartidaXadrez;
 import xadrez.PecaXadrez;
 
 public class Rei extends PecaXadrez {
 
-    public Rei( Tabuleiro tabuleiro,Cor cor) {
-        super(tabuleiro,cor );
+    private PartidaXadrez partidaXadrez;
+
+
+    public Rei(Tabuleiro tabuleiro, Cor cor, PartidaXadrez partidaXadrez) {
+        super(tabuleiro, cor);
+        this.partidaXadrez = partidaXadrez;
     }
 
 
@@ -17,63 +22,88 @@ public class Rei extends PecaXadrez {
         return "R";
     }
 
-    public boolean podeMover(Posicao posicao){
+    public boolean podeMover(Posicao posicao) {
         PecaXadrez pecaXadrez = (PecaXadrez) getTabuleiro().peca(posicao);
         return pecaXadrez == null || pecaXadrez.getCor() != getCor();
+    }
+
+    private boolean testeTorreRock(Posicao posicao) {
+        PecaXadrez pecaXadrez = (PecaXadrez) getTabuleiro().peca(posicao);
+        return pecaXadrez != null && pecaXadrez instanceof Torre && pecaXadrez.getCor() == getCor() && pecaXadrez.getContagemMovimento() == 0;
+
     }
 
     @Override
     public boolean[][] movimentoPossiveis() {
         boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
-        Posicao aux = new Posicao(0,0);
+        Posicao aux = new Posicao(0, 0);
 
         // cima
-        aux.setValores(posicao.getLinha()-1,posicao.getColuna());
-        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)){
-            mat[aux.getLinha()][aux.getColuna()] =true;
+        aux.setValores(posicao.getLinha() - 1, posicao.getColuna());
+        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) {
+            mat[aux.getLinha()][aux.getColuna()] = true;
         }
         // baixo
-        aux.setValores(posicao.getLinha()+1,posicao.getColuna());
-        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)){
-            mat[aux.getLinha()][aux.getColuna()] =true;
+        aux.setValores(posicao.getLinha() + 1, posicao.getColuna());
+        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) {
+            mat[aux.getLinha()][aux.getColuna()] = true;
         }
         // esquerda
-        aux.setValores(posicao.getLinha(),posicao.getColuna()-1);
-        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)){
-            mat[aux.getLinha()][aux.getColuna()] =true;
+        aux.setValores(posicao.getLinha(), posicao.getColuna() - 1);
+        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) {
+            mat[aux.getLinha()][aux.getColuna()] = true;
         }
         // direita
-        aux.setValores(posicao.getLinha(),posicao.getColuna()+1);
-        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)){
-            mat[aux.getLinha()][aux.getColuna()] =true;
+        aux.setValores(posicao.getLinha(), posicao.getColuna() + 1);
+        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) {
+            mat[aux.getLinha()][aux.getColuna()] = true;
         }
         // cima-esquerda
-        aux.setValores(posicao.getLinha()-1,posicao.getColuna()-1);
-        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)){
-            mat[aux.getLinha()][aux.getColuna()] =true;
+        aux.setValores(posicao.getLinha() - 1, posicao.getColuna() - 1);
+        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) {
+            mat[aux.getLinha()][aux.getColuna()] = true;
         }
         // cima-direita
-        aux.setValores(posicao.getLinha()-1,posicao.getColuna()+1);
-        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)){
-            mat[aux.getLinha()][aux.getColuna()] =true;
+        aux.setValores(posicao.getLinha() - 1, posicao.getColuna() + 1);
+        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) {
+            mat[aux.getLinha()][aux.getColuna()] = true;
         }
         // biaxo-esquerda
-        aux.setValores(posicao.getLinha()+1,posicao.getColuna()-1);
-        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)){
-            mat[aux.getLinha()][aux.getColuna()] =true;
+        aux.setValores(posicao.getLinha() + 1, posicao.getColuna() - 1);
+        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) {
+            mat[aux.getLinha()][aux.getColuna()] = true;
         }
         // baixo-direita
-        aux.setValores(posicao.getLinha()+1,posicao.getColuna()+1);
-        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)){
-            mat[aux.getLinha()][aux.getColuna()] =true;
+        aux.setValores(posicao.getLinha() + 1, posicao.getColuna() + 1);
+        if (getTabuleiro().posicaoExistente(aux) && podeMover(aux)) {
+            mat[aux.getLinha()][aux.getColuna()] = true;
         }
 
+        //Jogada Rock
 
+        if (getContagemMovimento() == 0 && !partidaXadrez.getCheck()) {
+            //Roque pequeno
+            Posicao auxTorre = new Posicao(posicao.getLinha(), posicao.getColuna() + 3);
+            if (testeTorreRock(auxTorre)) {
+                Posicao aux1 = new Posicao(posicao.getLinha(), posicao.getColuna() + 1);
+                Posicao aux2 = new Posicao(posicao.getLinha(), posicao.getColuna() + 2);
+                if (getTabuleiro().peca(aux1) == null && getTabuleiro().peca(aux2) == null) {
+                    mat[aux2.getLinha()][aux2.getColuna()] = true;
+                }
+            }
+            //Roque grande
+            Posicao auxTorre2 = new Posicao(posicao.getLinha(), posicao.getColuna() - 4);
+            if (testeTorreRock(auxTorre)) {
+                Posicao aux1 = new Posicao(posicao.getLinha(), posicao.getColuna() - 1);
+                Posicao aux2 = new Posicao(posicao.getLinha(), posicao.getColuna() - 2);
+                Posicao aux3 = new Posicao(posicao.getLinha(), posicao.getColuna() - 3);
+                if (getTabuleiro().peca(aux1) == null && getTabuleiro().peca(aux2) == null && getTabuleiro().peca(aux3) == null) {
+                    mat[aux2.getLinha()][aux2.getColuna()] = true;
+                }
 
+            }
 
-
-
-        return  mat;
+        }
+        return mat;
     }
-
 }
--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -98,6 +98,23 @@ public class PartidaXadrez {
             pecasTabuleiro.remove(pecaCapturada);
             pecasCapturadas.add(pecaCapturada);
         }
+        // Jogada Rock
+        // Rock pequeno
+         if (pecaMovida instanceof Rei && destino.getColuna()== origem.getColuna()+2){
+            Posicao origemTorre = new Posicao(origem.getLinha(),origem.getColuna()+3);
+            Posicao destinoTorre = new Posicao(origem.getLinha(),origem.getColuna()+1);
+            PecaXadrez torre = (PecaXadrez) tabuleiro.removePeca(origemTorre);
+            tabuleiro.colocarPecas(torre,destinoTorre);
+            torre.incrementarContagemMovimento();
+        }
+        //Rock grande
+        if (pecaMovida instanceof Rei && destino.getColuna()== origem.getColuna()-2){
+            Posicao origemTorre = new Posicao(origem.getLinha(),origem.getColuna()-4);
+            Posicao destinoTorre = new Posicao(origem.getLinha(),origem.getColuna()-1);
+            PecaXadrez torre = (PecaXadrez) tabuleiro.removePeca(origemTorre);
+            tabuleiro.colocarPecas(torre,destinoTorre);
+            torre.incrementarContagemMovimento();
+        }
         return pecaCapturada;
     }
 
@@ -110,6 +127,24 @@ public class PartidaXadrez {
             pecasCapturadas.remove(pecaCapturada);
             pecasTabuleiro.add(pecaCapturada);
         }
+        //Defazendo Rock pequeno
+        if (pecaMovida instanceof Rei && destino.getColuna()== origem.getColuna()+2){
+            Posicao origemTorre = new Posicao(origem.getLinha(),origem.getColuna()+3);
+            Posicao destinoTorre = new Posicao(origem.getLinha(),origem.getColuna()+1);
+            PecaXadrez torre = (PecaXadrez) tabuleiro.removePeca(destinoTorre);
+            tabuleiro.colocarPecas(torre,origemTorre);
+            torre.decrementarContagemMovimento();
+        }
+        //Defazendo Rock grande
+        if (pecaMovida instanceof Rei && destino.getColuna()== origem.getColuna()-2){
+            Posicao origemTorre = new Posicao(origem.getLinha(),origem.getColuna()-4);
+            Posicao destinoTorre = new Posicao(origem.getLinha(),origem.getColuna()-1);
+            PecaXadrez torre = (PecaXadrez) tabuleiro.removePeca(destinoTorre);
+            tabuleiro.colocarPecas(torre,origemTorre);
+            torre.decrementarContagemMovimento();
+        }
+
+
     }
 
     public void validarPosicaoOrigem(Posicao posicao) {
@@ -210,8 +245,8 @@ public class PartidaXadrez {
         colocarNovaPeca('a', 1, new Torre(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('b', 1, new Cavalo(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('c', 1, new Bispo(tabuleiro, Cor.BRANCO));
-        colocarNovaPeca('d', 5, new Dama(tabuleiro, Cor.BRANCO));
-        colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('d', 1, new Dama(tabuleiro, Cor.BRANCO));
+        colocarNovaPeca('e', 1, new Rei(tabuleiro, Cor.BRANCO,this));
         colocarNovaPeca('f', 1, new Bispo(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('g', 1, new Cavalo(tabuleiro, Cor.BRANCO));
         colocarNovaPeca('h', 1, new Torre(tabuleiro, Cor.BRANCO));
@@ -227,7 +262,7 @@ public class PartidaXadrez {
         colocarNovaPeca('a', 8, new Torre(tabuleiro, Cor.PRETO));
         colocarNovaPeca('b', 8, new Cavalo(tabuleiro, Cor.PRETO));
         colocarNovaPeca('c', 8, new Bispo(tabuleiro, Cor.PRETO));
-        colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO));
+        colocarNovaPeca('e', 8, new Rei(tabuleiro, Cor.PRETO,this));
         colocarNovaPeca('d', 8, new Dama(tabuleiro, Cor.PRETO));
         colocarNovaPeca('f', 8, new Bispo(tabuleiro, Cor.PRETO));
         colocarNovaPeca('g', 8, new Cavalo(tabuleiro, Cor.PRETO));
