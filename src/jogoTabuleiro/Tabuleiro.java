4456c991e8b498ebdfdffb9ac6106b66144f6721
Rafael P Jpsephino <Rafael.puckoski.josephino@gmail.com>
2021-08-20 22:38:44 -0300

    Excecoes na classe tabuleiro

--- src/jogoTabuleiro/Tabuleiro.java
+++ src/jogoTabuleiro/Tabuleiro.java
@@ -7,44 +7,63 @@ public class Tabuleiro {
     private Peca[][] pecas;
 
     public Tabuleiro(int linhas, int colunas) {
+
+        if (linhas < 1 || colunas < 1) {
+            throw new TabuleiroException("Erro na criacao do tabuleiro: Necessariominimo 1 Linha e 1 coluna ");
+        }
         this.linhas = linhas;
         this.colunas = colunas;
         pecas = new Peca[linhas][colunas];
-    }
 
+    }
 
     public int getLinhas() {
         return linhas;
-    }
-
-    public void setLinhas(int linhas) {
-        this.linhas = linhas;
     }
 
     public int getColunas() {
         return colunas;
     }
 
-    public void setColunas(int colunas) {
-        this.colunas = colunas;
-    }
-
     public Peca peca(int linha, int coluna) {
-
+        if (!posicaoExistente(linha,coluna)){
+            throw new TabuleiroException("Posicao nao existente no tabuleiro");
+        }
         return pecas[linha][coluna];
 
     }
 
     public Peca peca(Posicao posicao) {
-
+        if (!posicaoExistente(posicao)){
+            throw new TabuleiroException("Posicao nao existente no tabuleiro");
+        }
         return pecas[posicao.getLinha()][posicao.getColuna()];
 
     }
 
-    public void  colocarPecas(Peca peca, Posicao posicao){
+    public void colocarPecas(Peca peca, Posicao posicao) {
+        if (temPeca(posicao)){
+            throw new TabuleiroException("Ja exite uma peca na posicao: "+posicao);
+        }
         pecas[posicao.getLinha()][posicao.getColuna()] = peca;
         peca.posicao = posicao;
     }
 
+    private boolean posicaoExistente(int linha, int coluna) {
+        return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+    }
+
+    public boolean posicaoExistente(Posicao posicao) {
+
+        return posicaoExistente(posicao.getLinha(), posicao.getColuna());
+    }
+
+    public boolean temPeca(Posicao posicao) {
+        if (!posicaoExistente(posicao)){
+            throw new TabuleiroException("Posicao nao existente no tabuleiro");
+        }
+        return peca(posicao) != null;
+
+    }
 
 }
---
+++ src/jogoTabuleiro/TabuleiroException.java
@@ -0,0 +1,8 @@
+package jogoTabuleiro;
+
+public class TabuleiroException extends RuntimeException{
+
+    public TabuleiroException(String message) {
+        super(message);
+    }
+}
